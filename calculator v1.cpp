6626674d23e440dfa890c614b62bb745cc7e4deb
Ankit Tamrakar <atamroker94@gmail.com>
2025-03-19 09:30:38 +0545

    Only addition function

--- calculator v1.cpp
+++ calculator v1.cpp
@@ -28,17 +28,17 @@ int main() {
         case '+':
             result = num1 + num2;
             break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2 == 0) {
-                cout << "Error! Division by zero is not allowed." << endl;
-                return 1;
-            }
+        // case '-':
+        //     result = num1 - num2;
+        //     break;
+        // case '*':
+        //     result = num1 * num2;
+        //     break;
+        // case '/':
+        //     if (num2 == 0) {
+        //         cout << "Error! Division by zero is not allowed." << endl;
+            //     return 1;
+            // }
             result = num1 / num2;
             break;
         default:
