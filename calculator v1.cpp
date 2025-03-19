b0df9fb239ccbc01cf8f8a618adf1efdf0a5d7c9
Ankit Tamrakar <atamroker94@gmail.com>
2025-03-19 09:42:58 +0545

    Main File to be merged with otheres

--- calculator v1.cpp
+++ calculator v1.cpp
@@ -25,25 +25,25 @@ int main() {
     getValidNumber(num2);
 
     switch (operation) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2 == 0) {
-                cout << "Error! Division by zero is not allowed." << endl;
-                return 1;
-            }
-            result = num1 / num2;
-            break;
-        default:
-            cout << "Invalid operation!" << endl;
-            return 1;
+        // case '+':
+        //     result = num1 + num2;
+        //     break;
+        // case '-':
+        //     result = num1 - num2;
+        //     break;
+        // case '*':
+        //     result = num1 * num2;
+        //     break;
+        // case '/':
+        //     if (num2 == 0) {
+        //         cout << "Error! Division by zero is not allowed." << endl;
+        //         return 1;
+        //     }
+        //     result = num1 / num2;
+        //     break;
+        // default:
+        //     cout << "Invalid operation!" << endl;
+        //     return 1;
     }
     
     cout << "Result: " << result << endl;
