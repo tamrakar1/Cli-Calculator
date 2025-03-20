86cb08da20a9e7bf3e7225c710c16f1a62241850
Ankit Tamrakar <atamroker94@gmail.com>
2025-03-20 13:47:21 +0545

    division operation code added

--- calculator v1.cpp
+++ calculator v1.cpp
@@ -25,6 +25,15 @@ int main() {
     getValidNumber(num2);
 
     switch (operation) {   
+        case '+':
+        result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+       case '*':
+            result = num1 * num2;
+            break;
         case '/':
         if (num2 == 0) {
             cout << "Error! Division by zero is not allowed." << endl;
