868adfc9f41311eb4ea4493359fb89f07089eeba
Ankit Tamrakar <atamroker94@gmail.com>
2025-03-24 12:34:23 +0545

    New Branch text

--- calculator v1.cpp
+++ calculator v1.cpp
@@ -26,26 +26,8 @@ int main() {
 
     switch (operation) {   
         //"This is a random branch for test!"
+        //this is to notify that there is another branch for test!!!
         
-        case '+':
-        result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-       case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-        if (num2 == 0) {
-            cout << "Error! Division by zero is not allowed." << endl;
-            return 1;
-        }
-        result = num1 / num2;
-        break;
-    default:
-        cout << "Invalid operation!" << endl;
-        return 1;
     }
     
     cout << "Result: " << result << endl;
