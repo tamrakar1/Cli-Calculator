bde79f155249b52e4220b93e6e2d082b95022c44
Ankit Tamrakar <atamroker94@gmail.com>
2025-03-24 09:42:18 +0545

    Random text written added

--- calculator v1.cpp
+++ calculator v1.cpp
@@ -26,6 +26,26 @@ int main() {
 
     switch (operation) {   
         "This is a random branch for test!"
+        
+        case '+':
+        result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+       case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+        if (num2 == 0) {
+            cout << "Error! Division by zero is not allowed." << endl;
+            return 1;
+        }
+        result = num1 / num2;
+        break;
+    default:
+        cout << "Invalid operation!" << endl;
+        return 1;
     }
     
     cout << "Result: " << result << endl;
